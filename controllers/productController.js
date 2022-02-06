cf2c6ad69c071f6e5060257374a760ac9fc7c39b
utrox <89227600+utrox@users.noreply.github.com>
2022-02-06 11:26:29 +0100

    Implement database querying

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,8 +1,37 @@
+const { query } = require("express");
 const Product = require("../models/Product");
 const notFoundError = require("../utils/send-not-found");
 
 const getAllProducts = async (req, res) => {
-  const products = await Product.find({});
+  const { search, price, category, manufacturer, order_by } = req.query;
+  const queryParameters = {};
+
+  // if order_by parameter is not given, default sort by the most recently added Product.
+  var sortBy = order_by || "-_id";
+
+  if (search) {
+    queryParameters.title = { $regex: search, $options: "i" };
+  }
+
+  if (price) {
+    const [min, max] = price.split("-");
+    if (!min || !max || min > max) {
+      return res.status(400).json({
+        msg: "Invalid price query. Please use 'price=minPrice-maxPrice' format.",
+      });
+    }
+    queryParameters.price = { $gte: min, $lte: max };
+  }
+
+  if (category) {
+    queryParameters.category = category;
+  }
+
+  if (manufacturer) {
+    queryParameters.manufacturer = manufacturer;
+  }
+
+  const products = await Product.find(queryParameters).sort(sortBy);
   res.status(200).json({ noProducts: products.length, products });
 };
 
@@ -13,7 +42,11 @@ const createProduct = async (req, res) => {
 
 const getSingleProduct = async (req, res) => {
   const productID = req.params.id;
-  const product = await Product.findOne({ _id: productID }).populate("reviews");
+  // when requesting only one pro
+  const product = await Product.findOne({ _id: productID }).populate(
+    "reviews",
+    "title text username rating"
+  );
   if (!product) {
     return notFoundError(res, "Product", productID);
   }
